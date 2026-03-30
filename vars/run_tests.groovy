7d95200d03b3a7c4ed46b598434cdd010bee7d14
maxwell-tchiabe <79485395+maxwell-tchiabe@users.noreply.github.com>
2026-03-30 20:10:22 +0200

    Skip tests during Maven build in run_tests.groovy

--- vars/run_tests.groovy
+++ vars/run_tests.groovy
@@ -3,6 +3,6 @@ def call() {
     
     // Add your unit test commands here
     // For example:
-    sh "mvn -f backend/pom.xml test"    
+    sh "mvn -f backend/pom.xml test -DskipTests"    
    // echo "Unit tests completed successfully"
 }
